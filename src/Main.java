93bc3b50e406dced7444f820ffc9ba8118955410
AlexWorkPC <demyanalex@gmail.com>
2022-11-27 19:33:09 +0300

    задание 2-1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 2-1");
+        for (int year = 1904; year <2096 ; year=year+4) {
+            System.out.println(year+ " Является високосным годом ");
+
+        }
+    }
+}
