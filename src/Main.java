7b40a648114f0463e5a1a3a620b4000c769eb224
AlexWorkPC <demyanalex@gmail.com>
2022-11-27 19:50:28 +0300

    задание 2-3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 2-1");
@@ -8,6 +10,15 @@ public class Main {
         System.out.println(" Задание 2-2 ");
         for (int i = 7; i <=98 ; i=i+7) {
             System.out.print( i + " ");
+        }
+        //task3
+        System.out.println(" Задание 2-3 ");
+        int number =1;
+        System.out.print(number+" ");
+        for (int i = 0; i <9 ; i++) {
+            number=number*2;
+            System.out.print(number+" ");
+
         }
     }
 }
